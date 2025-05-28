e166915481ed67e8826ea1cd463532cff3d594fc
Miya Kuo <miya3927@gmail.com>
2025-05-27 17:16:24 -0700

    exercise 26 completed

--- exercises/ex26.js
+++ exercises/ex26.js
@@ -5,3 +5,12 @@ The Lumberjack - $9.95
 eggs, sausage, toast, hashbrowns, pancakes
 The object should contain properties for the name, price, and ingredients.
 */
+
+const breakfast = {
+    breakfastName: "The Lumberjack",
+    breakfastPrice: 9.95,
+    breakfastingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+}
+console.log(breakfast.breakfastName)
+console.log(breakfast.breakfastPrice)
+console.log(breakfast.breakfastingredients)
